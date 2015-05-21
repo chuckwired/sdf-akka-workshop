7826323de4349e2d993bf43df814e4faa4cb8b0d
Robert Sliwinski <robert.sliwinski@adstream.com>
2015-05-21 10:27:32 +0100

    changes #5

--- src/main/scala/com/boldradius/sdf/akka/StatsActor.scala
+++ src/main/scala/com/boldradius/sdf/akka/StatsActor.scala
@@ -19,4 +19,32 @@ class StatsActor extends Actor with ActorLogging {
     val count = for {browser <- Session.browsers} yield browser -> reqs.count(req => req.browser == browser)
     count.toMap
   }
+
+  def calculatePageVisitPercentage(reqs: List[Request]): Map[String,Int] = {
+    val visits: Map[String,Int] = calculateRequestsPerBrowser(reqs)
+    val totalVisits: Int = visits.map(el => el._2).sum
+    visits.map(element => (element._1 -> (element._2*100 / totalVisits)))
+  }
+
+  def top2browsers(reqs: List[Request]): List[String] = {
+    var browsers = calculateRequestsPerBrowser(reqs)
+    var top2 = List.empty[String]
+      top2 = top2 :+ browsers.maxBy(_._2)._1
+
+    browsers = browsers.filter(_._1 != top2.head)
+    top2 = top2 :+ browsers.maxBy(_._2)._1
+    top2
+  }
+
+  def top2referrers(reqs: List[Request]): List[String] = {
+    val countedReferrers = for {referrer <- Session.referrers} yield referrer -> reqs.count(req => req.referrer == referrer)
+    var referrers = countedReferrers.toMap
+
+    var top2 = List.empty[String]
+    top2 = top2 :+ referrers.maxBy(_._2)._1
+
+    referrers = referrers.filter(_._1 != top2.head)
+    top2 = top2 :+ referrers.maxBy(_._2)._1
+    top2
+  }
 }
--- src/test/scala/com/boldradius/sdf/akka/test/StatsActorSpec.scala
+++ src/test/scala/com/boldradius/sdf/akka/test/StatsActorSpec.scala
@@ -10,10 +10,13 @@ class StatsActorSpec extends BaseAkkaSpec {
       val statsActor = TestActorRef(StatsActor.props)
       val st: StatsActor = statsActor.underlyingActor
       val requestsPerBrowser = st.calculateRequestsPerBrowser(TestData.requests)
-
+      val visistsPercentage = st.calculatePageVisitPercentage(TestData.requests)
+      var top2browsers = st.top2browsers(TestData.requests)
+      var top2referrers = st.top2referrers(TestData.requests)
       val a = TestData.requests
 
       println(requestsPerBrowser)
+      println(visistsPercentage)
 //      rc.sessionStorage.size shouldBe 1
     }
   }
