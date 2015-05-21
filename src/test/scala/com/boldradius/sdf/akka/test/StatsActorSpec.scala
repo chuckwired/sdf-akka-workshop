eb1f73487c87b9bcd0057658ad3794c2286042fd
Robert Sliwinski <robert.sliwinski@adstream.com>
2015-05-21 10:58:13 +0100

    added tests

--- src/test/scala/com/boldradius/sdf/akka/test/StatsActorSpec.scala
+++ src/test/scala/com/boldradius/sdf/akka/test/StatsActorSpec.scala
@@ -1,28 +1,46 @@
 package com.boldradius.sdf.akka.test
 
+import akka.actor.ActorSystem
 import akka.testkit.TestActorRef
 import com.boldradius.sdf.akka._
 import com.boldradius.sdf.akka.test.TestData._
 
 
-class StatsActorSpec extends BaseAkkaSpec with StatsActorSetup {
+
+class StatsActorSpec extends BaseAkkaSpec {
+  trait StatsActorSetup {
+    val st = TestActorRef(StatsActor.props)
+    val statsActor: StatsActor = st.underlyingActor
+  }
+
   "The StatsActor" should {
-    "result in RequestConsumer with 1 sessionStorage element" in {
-      val statsActor = TestActorRef(StatsActor.props)
-      val st: StatsActor = statsActor.underlyingActor
-      val requestsPerBrowser = st.calculateRequestsPerBrowser(TestData.requests)
-      val visistsPercentage = st.calculatePageVisitPercentage(TestData.requests)
-      var top2browsers = st.top2browsers(TestData.requests)
-      var top2referrers = st.top2referrers(TestData.requests)
-
-      val a = TestData.requests
-
-      println(requestsPerBrowser)
-      println(visistsPercentage)
-//      rc.sessionStorage.size shouldBe 1
+    "calculate requests per browser" in new StatsActorSetup {
+      val requestsPerBrowser = statsActor.calculateRequestsPerBrowser(TestData.requests)
+      requestsPerBrowser("chrome") shouldBe 3
+      requestsPerBrowser("firefox") shouldBe 2
+      requestsPerBrowser("ie") shouldBe 1
     }
 
-    "calculate the busiest minute" in {
+    "calculate visits percentage per browser" in new StatsActorSetup {
+      val visistsPercentage = statsActor.calculatePageVisitPercentage(TestData.requests)
+      visistsPercentage("chrome") shouldBe 50
+      visistsPercentage("firefox") shouldBe 33
+      visistsPercentage("ie") shouldBe 16
+    }
+
+    "calculate top2 browsers" in new StatsActorSetup {
+      val top2browsers = statsActor.top2browsers(TestData.requests)
+      top2browsers(0) shouldBe "chrome"
+      top2browsers(1) shouldBe "firefox"
+    }
+
+    "calculate top2 referrers" in new StatsActorSetup {
+      val top2referrers = statsActor.top2referrers(TestData.requests)
+      top2referrers(0) shouldBe "google"
+      top2referrers(1) shouldBe "twitter"
+    }
+
+    "calculate the busiest minute" in new StatsActorSetup {
       val busiestMinute = statsActor.calculateBusiestMinute(requests)
       busiestMinute.minute shouldBe 23868
     }
